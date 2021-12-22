854305abff19d3525e5236bb8ce8b368e2bd33cf
Rolf Wang <j942856@yahoo.com>
2021-12-22 18:36:52 +0800

    css added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 var app=express();
 var PORT=process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended:true}));
+app.use('/css',express.static(__dirname +'/css'));
 
 
 app.get('/',function(req,res){
