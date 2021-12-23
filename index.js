fc1a1a9cae973b3a262a8fba11582c932a886b5a
Rolf Wang <j942856@yahoo.com>
2021-12-23 13:03:35 +0800

    add LTC and EURO/CSS added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use('/css',express.static(__dirname +'/css'));
 
 
 app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html')
+  res.sendFile(__dirname+'/index.html');
 })
 
 
@@ -26,14 +26,21 @@ app.post('/',function(req,res){
       'x-rapidapi-key': process.env.API_KEY,
       useQueryString: true
     }
-  };
+  }
+
+  ;
   request(options,function(error,response,body){
     if (error) throw new Error(error);
     var data=JSON.parse(body);
     var price=data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
     var currentDate=data['Realtime Currency Exchange Rate']['6. Last Refreshed'];
-    res.write("<p>The current date is "+currentDate+"</p>");
-    res.write("<h1>The current price of "+crypto+" is: "+price+fiat+"</h1>");
+    var filePath="css/pictures/rich.png";
+    res.write("<body style='background-color: #f9dca4;'>")
+    res.write("<p style='text-align:center;font-size:15pt;margin-top:100px;'>The current date is "+currentDate+"</p>");
+    res.write("<h1 style='text-align:center;font-size:30pt;'>The current price of "+crypto+" is: "+"<span style='color:red'>"+price+"</span>"+fiat+"</h1>");
+    res.write("<img style='margin-left:30%;width:50%;' src='"+filePath+"'/>");
+    res.write("</body>")
+
     res.send();
   })
   })
